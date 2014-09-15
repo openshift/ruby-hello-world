e810612c0d8087d778b834935797272ecf60e25d
Ben Parees <bparees@redhat.com>
2014-09-15 17:20:49 -0400

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a sample openshift v3 application repository.  
+
+For instructions on how to use it, please see: http://github.com/openshift/origin/blob/master/examples/ruby-sample
--- app.rb
+++ app.rb
@@ -3,9 +3,10 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 set :port,8080
 get '/' do
-  "Hello World2!
-User is #{ENV['ADMIN_USERNAME']}
-Password is #{ENV['ADMIN_PASSWORD']}
-DB password is #{ENV['DB_PASSWORD']}\n"
-
+  "Hello World!\n"+
+# ENV values are generated during template processing
+# and then passed to the container when openshift launches it.
+  "User is #{ENV['ADMIN_USERNAME']}\n"+
+  "Password is #{ENV['ADMIN_PASSWORD']}\n"+
+  "DB password is #{ENV['DB_PASSWORD']}\n"
 end
