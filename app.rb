39c89a5fbc7e986f477bff934af6ea5459d15e2e
Brenton Leanhardt <bleanhar@redhat.com>
2015-02-27 08:53:57 -0500

    Handling the DATABASE_TEST_SERVICE_HOST case

--- app.rb
+++ app.rb
@@ -27,7 +27,7 @@ end
 configure do
   puts "Run app..."
 
-  unless ENV["DATABASE_SERVICE_HOST"].nil?
+  unless ENV["DATABASE_SERVICE_HOST"].nil? && ENV["DATABASE_TEST_SERVICE_HOST"].nil?
     configure_database
   end
 end
