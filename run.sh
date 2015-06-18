10f7ebe653f48ece4e972a8ec26b673905b9b3e1
Ben Parees <bparees@redhat.com>
2015-06-18 14:39:20 -0400

    remove local db config

--- run.sh
+++ run.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 
-# Helper script to run the sample app locally, assumes a local mysql instance with a
-# login of root/root
-if [ -z "${MYSQL_DATABASE}" ]; then
-  export DATABASE_SERVICE_HOST=localhost
-  export DATABASE_SERVICE_PORT=3306
-  export MYSQL_DATABASE=test
-  export MYSQL_USER=root
-  export MYSQL_PASSWORD=root
-fi
-
 bundle exec ruby app.rb
